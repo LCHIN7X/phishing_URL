43ee655eefc2684999a6ecf9836e1da67e5846bb
LCHIN7X <lawchinxuan@gmail.com>
2025-03-18 00:33:06 +0800

    17

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pickle
+import pandas as pd
 
 
 app = Flask(__name__)
 
 file_path = "C:/Users/night/Documents/CS/phishing_URL/phishing_site_urls.csv"
+df = pd.read_csv(file_path,encoding="ISO-8859-1")
+
+
 
 @app.route('/')
 def home():
