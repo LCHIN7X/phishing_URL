27c32299a07acd72599ac7fd588b547a86d0decb
CHIN <165190113+LCHIN7X@users.noreply.github.com>
2025-06-29 19:52:17 +0800

    Update predictPhish.py

--- predictPhish.py
+++ predictPhish.py
@@ -1,23 +1,22 @@
 import pickle
 import numpy as np
 
-# Load trained model
+
 with open("phishing.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
-# Load trained CountVectorizer
+
 with open("vectorizer.pkl", "rb") as vec_file:
     vectorizer = pickle.load(vec_file)
 
 def phishPrediction(url):
-    # Transform input URL using the saved vectorizer
-    url_vectorized = vectorizer.transform([url])  # must be a list
+   
+    url_vectorized = vectorizer.transform([url]) 
 
-    # Predict label and probabilities
     y_pred = model.predict(url_vectorized)[0]
-    y_proba = model.predict_proba(url_vectorized)[0]  # [phishing_prob, non_phishing_prob]
+    y_proba = model.predict_proba(url_vectorized)[0]  
 
-    y_phishing = y_proba[0]  # Assuming label 'bad' corresponds to index 0
+    y_phishing = y_proba[0] 
     y_non_phishing = y_proba[1]
 
     print(f"Predicted label: {y_pred}")
